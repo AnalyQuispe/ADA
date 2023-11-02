077e8a8697871be406602579c57a6a9e26c8e8c5
Quispe Bejar Garlet Analy <64673450+AnalyQuispe@users.noreply.github.com>
2023-11-02 23:11:33 +0000

    Laboratorio06.

--- Laboratorio_06/Ejercicio02.cpp
+++ Laboratorio_06/Ejercicio02.cpp
@@ -22,50 +22,44 @@ Matrix subtract(Matrix A, Matrix B) {
     return C;
 }
 
+Matrix multiply2x2(Matrix A, Matrix B) {
+    Matrix C;
+    C.m[0][0] = A.m[0][0] * B.m[0][0] + A.m[0][1] * B.m[1][0];
+    C.m[0][1] = A.m[0][0] * B.m[0][1] + A.m[0][1] * B.m[1][1];
+    C.m[1][0] = A.m[1][0] * B.m[0][0] + A.m[1][1] * B.m[1][0];
+    C.m[1][1] = A.m[1][0] * B.m[0][1] + A.m[1][1] * B.m[1][1];
+    return C;
+}
+
 Matrix strassen(Matrix A, Matrix B) {
-    // Base case: 2x2 matrix
-    if (sizeof(A.m)/sizeof(A.m[0]) == 2) {
-        // Código simplificado para matrices 2x2, similar al que proporcionaste originalmente
-        Matrix C;
-        int m1 = (A.m[0][0] + A.m[1][1]) * (B.m[0][0] + B.m[1][1]);
-        int m2 = (A.m[1][0] + A.m[1][1]) * B.m[0][0];
-        int m3 = A.m[0][0] * (B.m[0][1] - B.m[1][1]);
-        int m4 = A.m[1][1] * (B.m[1][0] - B.m[0][0]);
-        int m5 = (A.m[0][0] + A.m[0][1]) * B.m[1][1];
-        int m6 = (A.m[1][0] - A.m[0][0]) * (B.m[0][0] + B.m[0][1]);
-        int m7 = (A.m[0][1] - A.m[1][1]) * (B.m[1][0] + B.m[1][1]);
+    // Esta versión asume que las matrices son 4x4 y las descompone en submatrices 2x2
 
-        C.m[0][0] = m1 + m4 - m5 + m7;
-        C.m[0][1] = m3 + m5;
-        C.m[1][0] = m2 + m4;
-        C.m[1][1] = m1 - m2 + m3 + m6;
-
-        return C;
+    // Calcula las submatrices 2x2 de A y B
+    Matrix A11, A12, A21, A22, B11, B12, B21, B22;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            A11.m[i][j] = A.m[i][j];
+            A12.m[i][j] = A.m[i][j+2];
+            A21.m[i][j] = A.m[i+2][j];
+            A22.m[i][j] = A.m[i+2][j+2];
+            
+            B11.m[i][j] = B.m[i][j];
+            B12.m[i][j] = B.m[i][j+2];
+            B21.m[i][j] = B.m[i+2][j];
+            B22.m[i][j] = B.m[i+2][j+2];
+        }
     }
 
-    // Descomponer cada matriz 4x4 en 4 submatrices 2x2
-    // (Esto se puede hacer de manera más eficiente, pero por simplicidad lo haré manualmente)
+    // Aplica Strassen a las submatrices 2x2
+    Matrix C11 = add(multiply2x2(A11, B11), multiply2x2(A12, B21));
+    Matrix C12 = add(multiply2x2(A11, B12), multiply2x2(A12, B22));
+    Matrix C21 = add(multiply2x2(A21, B11), multiply2x2(A22, B21));
+    Matrix C22 = add(multiply2x2(A21, B12), multiply2x2(A22, B22));
 
-    Matrix A11 = {{ {A.m[0][0], A.m[0][1]}, {A.m[1][0], A.m[1][1]} }};
-    Matrix A12 = {{ {A.m[0][2], A.m[0][3]}, {A.m[1][2], A.m[1][3]} }};
-    Matrix A21 = {{ {A.m[2][0], A.m[2][1]}, {A.m[3][0], A.m[3][1]} }};
-    Matrix A22 = {{ {A.m[2][2], A.m[2][3]}, {A.m[3][2], A.m[3][3]} }};
-
-    Matrix B11 = {{ {B.m[0][0], B.m[0][1]}, {B.m[1][0], B.m[1][1]} }};
-    Matrix B12 = {{ {B.m[0][2], B.m[0][3]}, {B.m[1][2], B.m[1][3]} }};
-    Matrix B21 = {{ {B.m[2][0], B.m[2][1]}, {B.m[3][0], B.m[3][1]} }};
-    Matrix B22 = {{ {B.m[2][2], B.m[2][3]}, {B.m[3][2], B.m[3][3]} }};
-
-    // Recursivamente multiplicar submatrices 2x2
-    Matrix C11 = add(strassen(A11, B11), strassen(A12, B21));
-    Matrix C12 = add(strassen(A11, B12), strassen(A12, B22));
-    Matrix C21 = add(strassen(A21, B11), strassen(A22, B21));
-    Matrix C22 = add(strassen(A21, B12), strassen(A22, B22));
-
-    // Construir matriz resultante 4x4 a partir de los resultados 2x2
+    // Construye la matriz resultante 4x4
     Matrix C;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
             C.m[i][j] = C11.m[i][j];
             C.m[i][j+2] = C12.m[i][j];
             C.m[i+2][j] = C21.m[i][j];
